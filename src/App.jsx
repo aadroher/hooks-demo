543feec23705620eb7cdc65b6897eddcc3352846
Armand Adroher <aadroher@gmail.com>
2019-08-12 21:03:00 +0100

    WIP.

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,6 @@
 import React, { useState } from 'react';
-import Heading from './components/heading';
-import CurrencyForm from './components/currency-form';
-import ConversionTable from './components/conversion-table';
-import { getCurrencies } from './utils/currencies';
+import FunctionalApp from './components/functional-app';
+import StatefulApp from './components/stateful-app';
 import './App.css';
 
-const fetchRates = async currencyCode => {
-  if (currencyCode) {
-    const response = await fetch(`/latest?base=${currencyCode}`);
-    if (response.ok) {
-      return response.json();
-    } else {
-      throw new Error(`${response.status} - ${response.statusText}`);
-    }
-  }
-  return {
-    rates: [],
-  };
-};
-
-const App = () => {
-  const currencies = getCurrencies();
-  const [state, setState] = useState({
-    selectedCurrency: '',
-    amount: 1,
-    rates: {},
-  });
-  const { selectedCurrency, amount, rates } = state;
-
-  return (
-    <div className="app">
-      <Heading />
-      <CurrencyForm
-        currencies={currencies}
-        selectedCurrency={selectedCurrency}
-        onChangeCurrencySelection={async ({
-          target: { value: selectedCurrency },
-        }) => {
-          const { rates } = await fetchRates(selectedCurrency);
-          setState(prevState => ({
-            ...prevState,
-            selectedCurrency,
-            rates,
-          }));
-        }}
-        amount={amount}
-        onChangeAmount={({ target: { value: amount } }) => {
-          console.log({ amount });
-          setState(prevState => ({
-            ...prevState,
-            amount,
-          }));
-        }}
-      />
-      <ConversionTable
-        currencies={currencies}
-        selectedCurrency={selectedCurrency}
-        amount={amount}
-        rates={rates}
-      />
-    </div>
-  );
-};
-
-export default App;
+export default FunctionalApp;
---
+++ src/components/stateful-app.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import Heading from './heading';
+import CurrencyForm from './currency-form';
+import ConversionTable from './conversion-table';
+import { getCurrencies } from '../utils/currencies';
+
+const fetchRates = async currencyCode => {
+  if (currencyCode) {
+    const response = await fetch(`/latest?base=${currencyCode}`);
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error(`${response.status} - ${response.statusText}`);
+    }
+  }
+  return {
+    rates: [],
+  };
+};
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedCurrency: '',
+      amount: 1,
+      rates: {},
+    };
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <Heading />
+        <CurrencyForm
+          currencies={currencies}
+          selectedCurrency={selectedCurrency}
+          onChangeCurrencySelection={async ({
+            target: { value: selectedCurrency },
+          }) => {
+            const { rates } = await fetchRates(selectedCurrency);
+            this.setState({
+              selectedCurrency,
+              rates,
+            });
+          }}
+          amount={amount}
+          onChangeAmount={({ target: { value: amount } }) => {
+            this.setState({
+              ...prevState,
+              amount,
+            });
+          }}
+        />
+        <ConversionTable
+          currencies={currencies}
+          selectedCurrency={selectedCurrency}
+          amount={amount}
+          rates={rates}
+        />
+      </div>
+    );
+  }
+}
